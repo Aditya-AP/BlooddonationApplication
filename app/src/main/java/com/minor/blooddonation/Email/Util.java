5314c110d9fcde3a99c5061419c42a8006f83e8b
Aditya gupta <Adityagupta7019@gmail.com>
2024-06-14 15:11:59 +0530

    Complete Project

---
+++ app/src/main/java/com/minor/blooddonation/Email/Util.java
@@ -0,0 +1,7 @@
+package com.minor.blooddonation.Email;
+
+public class Util {
+
+    public static final String EMAIL = "raghugaurav9889@gmail.com";
+    public static final String PASSWORD = "Gaurav@2000";
+}
